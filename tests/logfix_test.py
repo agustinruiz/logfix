727461250209a62c281fe3ca969ce3eba961f23d
Agustin Ruiz <agustinomarruiz@gmail.com>
2023-08-18 20:09:49 -0300

    tests improvements

--- tests/logfix_test.py
+++ tests/logfix_test.py
@@ -1,8 +1,32 @@
+import unittest;
 import os;
 import sys;
-sys.path.append('./src/')
+sys.path.append('./src/');
 
 from logfix import logfix_process;
 
-logfix_process(os.getcwd() + "/samples/SWAP-log", "$ASHTP");
-logfix_process(os.getcwd() + "/samples/SWAP-log132", "$ASPXY");
+class TestMainFunctions(unittest.TestCase):
+
+    SWAP_LOG_FILE = os.getcwd() + "/samples/SWAP-log";
+    SWAP_LOG132_FILE = os.getcwd() + "/samples/SWAP-log132";
+    OUTPUT_SWAP_LOG_FILE = SWAP_LOG_FILE + "-fixed";
+    OUTPUT_SWAP_LOG132_FILE = SWAP_LOG132_FILE + "-fixed";
+
+    def test_output_swap_file_creation(self):
+        if os.path.exists(f"{self.OUTPUT_SWAP_LOG_FILE}"):
+            os.remove(self.OUTPUT_SWAP_LOG_FILE);
+        
+        logfix_process(self.SWAP_LOG_FILE, "$ASHTP");
+
+        self.assertTrue(os.path.exists(f"{self.OUTPUT_SWAP_LOG_FILE}"), f"File '{self.OUTPUT_SWAP_LOG_FILE}' does not exist");
+    
+    def test_output_swap_132_file_creation(self):
+        if os.path.exists(f"{self.OUTPUT_SWAP_LOG132_FILE}"):
+            os.remove(self.OUTPUT_SWAP_LOG132_FILE);
+        
+        logfix_process(self.SWAP_LOG132_FILE, "$ASPXY");
+
+        self.assertTrue(os.path.exists(f"{self.OUTPUT_SWAP_LOG132_FILE}"), f"File '{self.OUTPUT_SWAP_LOG132_FILE}' does not exist");
+
+if __name__ == '__main__':
+    unittest.main();
